fbc57f44f14704e93b3260a89d1b5cef481e8d82
François ROLAND <francois.roland@umons.ac.be>
2018-06-02 18:25:47 +0200

    Initiate project from start.spring.io

---
+++ src/main/java/be/fablabmons/falamasuki/FalamasukiApplication.java
@@ -0,0 +1,12 @@
+package be.fablabmons.falamasuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalamasukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalamasukiApplication.class, args);
+	}
+}
