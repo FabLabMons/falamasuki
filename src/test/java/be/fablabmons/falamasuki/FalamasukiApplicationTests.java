418bc185d08ce941fe7390399c0d3d055341130f
François ROLAND <francois.roland@umons.ac.be>
2018-06-02 20:38:32 +0200

    Format Java files according to code style.

--- src/test/java/be/fablabmons/falamasuki/FalamasukiApplicationTests.java
+++ src/test/java/be/fablabmons/falamasuki/FalamasukiApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FalamasukiApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
